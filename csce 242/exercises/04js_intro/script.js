d335e13a7fa25ffd299a8ceaa305d80db3ab2bcf
ashtonsears <ashtonesears@gmail.com>
2025-02-05 11:18:23 -0500

    fixed wireframe to be publicly viewed

---
+++ csce 242/exercises/04js_intro/script.js
@@ -0,0 +1,10 @@
+//function with arrow notation
+/*const doStuff =  () => {
+    console.log("doing stuff");
+}*/
+
+//do stuff when button clicked - anonymous function
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("WOW!");
+};
+//Parentheses mean now
